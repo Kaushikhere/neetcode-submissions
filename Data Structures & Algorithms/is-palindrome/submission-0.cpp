109893ef06b3a5ae7f5cd6e1312a90c68cd84c5f
Kaushikhere <99199075+Kaushikhere@users.noreply.github.com>
2026-08-08 19:32:26 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int n = s.size();
+        string t = "";
+
+        for(int i = 0; i < s.size(); i++) {
+            if(isalnum(s[i])) {
+            t.push_back(tolower(s[i]));
+            }
+        }
+
+        int i = 0; 
+        int j = t.size() - 1;
+
+        while(i < j) {
+            if(t[i] != t[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
